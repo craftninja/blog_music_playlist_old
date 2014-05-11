619746031d883da96cb058bac6c0cefb32f9d6a2
EmilyPlatzer <emilyplatzer@gmail.com>
2014-05-11 13:50:12 -0600

    User can edit song title and url

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   get '/playlist' => 'playlist#index'
   get '/playlist/new' => 'playlist#new'
   post '/playlist' => 'playlist#create'
+  get '/playlist/:id' => 'playlist#show'
+  get '/playlist/:id/edit' => 'playlist#edit'
+  put '/playlist/:id' => 'playlist#update'
+
+  resources :thing
 
 end
